930c40c89ab9bfe0f302b800b98822874f294897
Viktor Churikov <scanlave@gmail.com>
2022-05-26 16:37:47 +0300

    Word counter
    Implementation of word count in page text

--- viceversa/viceversa/views.py
+++ viceversa/viceversa/views.py
@@ -8,8 +8,10 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
+    count_word = len(user_text.split(' '))
     return render(request,
                   'reverse.html',
-                  {'usertext': user_text,
-                   'reversedtext': reversed_text}
+                  {'userText': user_text,
+                   'reversedText': reversed_text,
+                   'countWord': count_word}
                   )
